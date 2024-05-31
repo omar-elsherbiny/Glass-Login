ef0e420712daff609d89ad4b94e29a182759601a
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-06-01 01:40:26 +0300

    beggining info panel

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ toggle.addEventListener('click', function () {
     localStorage.setItem('theme', targetTheme);
 });
 // theme switch
+
+const infoPanel = document.getElementById('info-panel')
+const loginPanel = document.getElementById('login-panel');
+const signupPanel = document.getElementById('signup-panel');
+const login2register = document.getElementById('login-to-register');
+const register2login = document.getElementById('register-to-login');
+
+login2register.addEventListener('click', event => {
+    infoPanel.style.left = '65%';
+    loginPanel.style.width = '35%';
+    loginPanel.style.opacity = '0';
+    signupPanel.style.width = '65%';
+    signupPanel.style.opacity = '1';
+});
