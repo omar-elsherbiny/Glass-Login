18b5568e16efa6fa53f793a2256c3f509f1c32e9
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-05-31 23:11:39 +0300

    light dark theme

---
+++ script.js
@@ -0,0 +1,21 @@
+let toggle = document.getElementById('theme_toggle');
+let storedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+if (storedTheme) {
+    document.documentElement.setAttribute('data-theme', storedTheme)
+    if (storedTheme === 'dark') {
+        toggle.checked = true;
+    }
+    else {
+        toggle.checked = false;
+    }
+}
+toggle.addEventListener('click', function () {
+    let currentTheme = document.documentElement.getAttribute('data-theme');
+    let targetTheme = 'light';
+    if (currentTheme === 'light') {
+        targetTheme = 'dark';
+    }
+    document.documentElement.setAttribute('data-theme', targetTheme);
+    localStorage.setItem('theme', targetTheme);
+});
+// theme switch
